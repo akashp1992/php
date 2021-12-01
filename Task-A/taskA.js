f5834765d84d583a81d877eeef0d946485d6c77c
vasuDave <vdave593@gmail.com>
2021-12-01 19:18:49 +0530

    init commit

---
+++ Task-A/taskA.js
@@ -0,0 +1,29 @@
+function checkMethod()
+{
+var format;
+var radioFormat = document.getElementsByName('format');
+for (var b = 0, length = radioFormat.length; b < length; b++)
+{
+    if(radioFormat[b].checked)
+    {
+        format = radioFormat[b].value;
+        console.log(format);
+        break;
+    }
+}    
+    var xhttp = new XMLHttpRequest();
+    var currFrom = document.getElementsByName('from')[0].value;
+    var currTo = document.getElementsByName('to')[0].value;
+    var amnt = document.getElementsByName('amnt')[0].value;
+    xhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200) 
+        {
+            console.log(this.responseText);
+            document.getElementById("resultbox").value = this.responseText;
+        }
+    };
+    xhttp.open("GET", "../index.php?from="+currFrom+"&to="+currTo+"&amnt="+amnt+"&format="+format,true);
+    xhttp.send();
+
+}
